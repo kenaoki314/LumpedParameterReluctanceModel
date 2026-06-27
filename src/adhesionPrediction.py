89c30a6fe52e0447c122f79308a50366af81d794
kenaoki314 <kenaoki@hawaii.edu>
2026-06-26 18:27:09 -1000

    hello carla

--- src/adhesionPrediction.py
+++ src/adhesionPrediction.py
@@ -7,8 +7,39 @@ assumption that the magnets are in a parallel configuration and that the magneti
 The model also assumes that the magnets are in a vacuum and that there are no other magnetic materials present. 
 The model simplifes the magnetic system into its equivealnt magnetic cicuit and uses the reluctance of the magnetic circuit to calculate the adhesion force."""
 
-def internal_reluctance ()
+def internal_reluctance (magnet_length: float, mu_0: float, mu_r: float, Pole_Area: float) -> float:
     """will calculate the internal reluctance of the magnetic circuit 
     args: 
     l_m : magnet length in magnetization direction [m]
     A_m : magnet cross sectional area [m^2]
+    mu: magnetic permeability of the magnet [H/m] 
+    returns: 
+    R_m: internal reluctance of the magnet [H^-1]"""
+    R_m = magnet_length / (mu_r * mu_0 * Pole_Area)
+    return R_m 
+def airgap_reluctance(airgap: float, mu_0: float, A_m: float):
+    """ will calcuate the reluctance due to the air gap
+    args: 
+    x: airgap of pole face and iron plate [m]
+    mu_0 permeability of free space
+    A_m cross sectional area [m^2]
+    returns:
+    R_g: reluctance due to air gap H/m"""
+    R_g = airgap / (mu_0 * A_m)
+    return R_g 
+def flux_calculation(R_g: float, R_m: float, MMF: float) -> float: 
+    """use ohms law equivalent to calculate the magnetic flux using MMF = flux * reluctance
+    args: 
+    MMF = magnetomotive force [H]
+    R_g 
+    R_m 
+    return: 
+    flux    """
+    R_total = R_g + R_m 
+    Magnetic_Flux = MMF / R_total 
+    return Magnetic_Flux 
+
+def Adhesion_Calculation(Magnetic_Flux: float, mu_0: float, Pole_Area: float) -> float: 
+    "get force from flux mu and area"
+    AdhesionForce = Magnetic_Flux**2 / (2 * mu_0 * Pole_Area)
+    return AdhesionForce 
---
+++ src/curveFitting.py
@@ -0,0 +1,5 @@
+C = 'carla' 
+print(f'hello {C}')
+
+
+
